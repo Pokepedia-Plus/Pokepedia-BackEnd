18f84e99102bb1f7c1d4de6a96a605ac8fcabfd7
damiankohut <damiankohut@gmail.com>
2023-04-03 23:10:28 -0400

    added node-fetch

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const pool = require("./db");
+const fetch = require('node-fetch');
 // const AuthRouter = require("./routes/router");
 
 //middleware
