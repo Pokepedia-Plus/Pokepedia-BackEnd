1f055b0670213df71bd0763e72cfa714740f48e5
damiankohut <damiankohut@gmail.com>
2023-04-03 23:41:53 -0400

    minor changes

--- knexfile.js
+++ knexfile.js
@@ -1,25 +1,36 @@
 // Update with your config settings.
-
+require("dotenv").config();
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
 module.exports = {
-
-    development: {
-      client: 'postgresql',
-      connection: {
-        database: "pokepedia",
-        user: "postgres",
-        password: "damian",
-      },
-      pool: {
-        min: 2,
-        max: 10
-      },
-      migrations: {
-        tableName: 'knex_migrations'
-      }
-    }
-  
-  };
-  
+  development: {
+    client: "postgresql",
+    connection: {
+      database: process.env.DATABASE,
+      user: process.env.USER,
+      password: process.env.PG_SECRET,
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+  },
+  production: {
+    client: "pg",
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false },
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+  },
+};
